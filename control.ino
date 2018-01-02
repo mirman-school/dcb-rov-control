ad3f8897db19e48d43b1a64a33bb26817067a27d
mirman-mtaggart <mtaggart@mirman.org>
2018-01-02 15:55:19 -0800

    Modified turn code

--- control.ino
+++ control.ino
@@ -6,16 +6,11 @@ int rate2 = 0; // motor 2 speed
 int rateMod1 = 0; // motor 1 speed modifier
 int rateMod2 = 0; // motor 2 speed modifier
 
-
 /*
-Here we're going to create some helper variables to name the direction instructions.
+The null zone gives a little more zero for stop.
 */
-String AHEAD = "AHEAD";
-String BACK = "BACK";
-String PORT = "PORT";
-String STARBOARD = "STARBOARD";
-String STOP = "STOP";
-
+int nullMin = 490;
+int nullMax = 510;
 
 /* 
 Software definitions for both motors so we can control them
@@ -45,40 +40,24 @@ void loop() {
     // Get stickX/stickY joystick values
     stickX = analogRead(A0);
     stickY = analogRead(A1);
-    rate1 = map(stickX, 501, 1023, 0, 255);
-    rate2 = map(stickY, 501, 1023, 0, 255);
-    rateMod1 = map(stickX, 509, 1023, 0, 255);
-    rateMod2 = map(stickY, 0, 509, 255, 0);
 
-    // Turn these numbers into a simple direction command
-    String direction = translate();
+    // Turn raw stick input into vectors for the motors
+    vecX = normalize(stickX, 0, 1023, 0, 255);
+    vecY = normalize(stickY, 0, 1023, 0, 255);
 
-    /*
-    When we have a set of known values for a variable and a long conditional, we can use a switch-case structure instead of if-else trees.
-    */
-    
-    switch (direction) {
-        case AHEAD:
-            motor1.run(FORWARD);
-            motor2.run(FORWARD);
-            break;
-        case BACK:
-            motor1.run(BACKWARD);
-            motor2.run(BACKWARD);
-            break;
-        case PORT:
-            motor1.run(FORWARD);
-            motor2.run(FORWARD);
-            break;
-        case STARBOAD:
-            motor1.run(FORWARD);
-            motor2.run(FORWARD);
-            break;
-        default:
-            break;
+    // Steering instructions
+    if (vecX == 0 && vecY == 0) {
+        stop();  
+    } else if (vecX == 0) {
+        if (vecY >= 128 ) {
+            forward();
+        } else {
+            reverse();
+        }
+    } else {
+        turn();
     }
-
-
+    
     // Print the data from the device
     Serial.print(stickX);
     Serial.print('-');
@@ -92,25 +71,57 @@ void loop() {
     Serial.println(rateMod2);
 }
 
-// Turn numbers into a direction command
-String translate() {
-
-    int speedX = constrain((rate1-rateMod1), 0, 255);
-    int speedY = constrain((rate2+rateMod2), 0, 255);
-    motor1.setSpeed(speedX);
-    motor2.setSpeed(speedY);
-
-    if(stickX < 501 || stickY < 509 || stickY > 509){
-        return FORWARD;
-    } else if (stickX > 501){
-        return BACKWARD;
+int normalize(int stick) {
+    if (stick >= nullMin && stick <= nullMax) {
+        return 0;
+    } else if (stick < 511) {
+        return map(stick, 0, 511, 0, 255);
     } else {
-        return STOP;
+        return map(stick, 512, 254, 0, 255);
     }
 }
 
-int treatValue(int data) {
-  return (data * 9 / 1024) + 48;
+/*
+Helper functions for managing the motors
+*/
+
+void stop() {
+    motor1.setSpeed(0);
+    motor1.setSpeed(0);
 }
 
 
+void forward() {
+    motor1.setSpeed(vecY);
+    motor2.setSpeed(vecY);
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+}
+
+void reverse() {
+    motor1.setSpeed(vecY);
+    motor2.setSpeed(vecY);
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+}
+
+void turn() {
+    /*
+    Basically, a turn is a reversal of one engine, and then turning that engine at X rate
+    */
+
+
+    // Turning starboard
+    if (stickX >= 128) {
+        motor1.setSpeed(vecY);
+        motor2.setSpeed(vecX);
+        motor1.run(FORWARD);
+        motor2.run(BACKWARD);
+    // Turning port
+    } else {
+        motor1.setSpeed(vecX);
+        motor2.setSpeed(vecY);
+        motor1.run(BACKWARD);
+        motor2.run(FORWARD);
+    }
+}
